8e1a9b67ff4d18fba73dec540a854963f1cf502b
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2022-05-18 13:48:31 -0500

    added auth & other things

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
+  /*
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
+  );*/
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>We now have Auth!</h1>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
